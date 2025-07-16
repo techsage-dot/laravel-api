6cf3cdda35a221bfed51c343fd51dd9ba95ece3f
Bhargav <bhautiknimavat75@gmail.com>
2025-07-16 22:30:00 +0530

    Add test API route

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,10 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API is working! 🚀',
+        'status' => true
+    ]);
+});
